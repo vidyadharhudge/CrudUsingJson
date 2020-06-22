4b1c11e4349ef6bfbf11dd765ec8bb64a66892c3
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-06-22 05:14:05 -0700

    Added:-Welcome Message

---
+++ src/main/java/com/example/crudusingjson/CrudUsingJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudusingjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsingJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudUsingJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudusingjson/CrudUsingJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudusingjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsingJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
